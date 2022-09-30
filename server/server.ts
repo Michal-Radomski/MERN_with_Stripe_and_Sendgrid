7a117b93b04cf4b76c8a101dbbeedd872c461b64
Michal Radomski <michalra@op.pl>
2022-10-01 00:43:17 +0200

    Server - initial setup

---
+++ server/server.ts
@@ -0,0 +1,43 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+import express, { Express, Request, Response } from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+
+// Import routes
+
+// The server
+const app: Express = express();
+
+// Middlewares
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("combined"));
+
+//Route middleware
+
+// Mongo DB
+mongoose
+  .connect(process.env.MONGO_URL as string)
+  .then((con: { connection: { host: string } }) => {
+    console.log(`MongoDB Database connected with HOST: ${con.connection.host}`);
+  })
+  .catch((error: string) => console.log("Mongo DB Error => ", error));
+
+// Test route
+app.get("/", (req: Request, res: Response) => {
+  console.log("req.ip:", req.ip);
+  res.send("<h1 style='color:blue;text-align:center'>API is running</h1>");
+});
+
+// Port
+const port = (process.env.PORT || 5000) as number;
+
+app.listen({ port: port }, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+  // For testing only
+  console.log("Current Time:", new Date().toLocaleTimeString());
+});
