3343940d087fa09defdf45586256668e37adf3f5
Michal Radomski <michalra@op.pl>
2022-10-02 13:55:09 +0200

    Stripe payment without customer done

---
+++ server/Interfaces.d.ts
@@ -0,0 +1,5 @@
+// Types and Interfaces
+
+export interface CustomError extends Error {
+  message: string;
+}
--- server/controller.ts
+++ server/controller.ts
@@ -1,13 +1,11 @@
 import { Request, Response } from "express";
 import { v4 as uuidv4 } from "uuid";
+import { CustomError } from "./Interfaces";
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY, {
   apiVersion: "2022-08-01",
 });
 
-interface ExtendedObject extends Object {
-  id: string;
-}
-
+// //* V2 - Stripe payment without customer creation
 export const stripePayment = async (req: Request, res: Response) => {
   console.log("req.ip:", req.ip);
   const { product, token, price } = req.body;
@@ -16,51 +14,107 @@ export const stripePayment = async (req: Request, res: Response) => {
   const idempotencyKey = uuidv4();
   // console.log({ idempotencyKey });
 
-  return await stripe.customers
-    .create(
+  try {
+    const charge = await stripe.charges.create(
       {
-        email: token.email,
+        amount: price * 100,
+        currency: "pln",
         source: token.id,
-      },
-      {
-        timeout: 5000, // 5 seconds
-        maxNetworkRetries: 2,
-      }
-    )
-    .then((customer: ExtendedObject) => {
-      // console.log({ customer });
-      const charge = stripe.charges
-        .create(
-          {
-            amount: price * 100,
-            currency: "pln",
-            customer: customer.id,
-            receipt_email: token.email,
-            description: `purchase of ${product}`,
-            shipping: {
-              name: token.card.name,
-              address: {
-                country: token.card.address_country,
-              },
-            },
+        receipt_email: token.email,
+        description: `purchase of ${product}`,
+        shipping: {
+          name: token.card.name,
+          address: {
+            country: token.card.address_country,
           },
-          { idempotencyKey }
-        )
-        .then((charge: any) => {
-          console.log({ charge });
-        });
-      if (!charge) {
-        throw new Error("charge unsuccessful");
-      }
-    })
-    .then((result: Object) => {
-      // console.log({ result });
-      res.status(200).json(result);
-    })
-    .catch((error: string) => {
-      console.log({ error });
-      res.status(500).json({
-        error,
-      });
+        },
+      },
+      { idempotencyKey }
+    );
+    if (!charge) {
+      console.log("Charge unsuccessful");
+      throw new Error("Charge unsuccessful");
+    }
+    // console.log("charge:", charge);
+
+    const response = {
+      idempotencyKey: idempotencyKey,
+      id: charge?.id,
+      amount_captured: charge?.amount_captured,
+      currency: charge?.currency,
+      created: charge?.created,
+      paid: charge?.paid,
+      receipt_email: charge?.receipt_email,
+      receipt_url: charge?.receipt_url,
+      name: charge?.source?.name,
+    };
+
+    res.status(200).json({ response: response, message: "Charge posted successfully" });
+  } catch (error) {
+    res.status(500).json({
+      message: (error as CustomError).message,
     });
+  }
 };
+
+// interface ExtendedObject extends Object {
+//   id: string;
+// }
+
+// // //* V1 - Stripe payment with customer creation
+// export const stripePayment = async (req: Request, res: Response) => {
+//   console.log("req.ip:", req.ip);
+//   const { product, token, price } = req.body;
+//   // console.log({ product, token, price });
+//   // console.log("price:", price, typeof price);
+//   const idempotencyKey = uuidv4();
+//   // console.log({ idempotencyKey });
+
+//   return await stripe.customers
+//     .create(
+//       {
+//         email: token.email,
+//         source: token.id,
+//       },
+//       {
+//         timeout: 5000, // 5 seconds
+//         maxNetworkRetries: 2,
+//       }
+//     )
+//     .then((customer: ExtendedObject) => {
+//       // console.log({ customer });
+//       const charge = stripe.charges
+//         .create(
+//           {
+//             amount: price * 100,
+//             currency: "pln",
+//             customer: customer.id,
+//             receipt_email: token.email,
+//             description: `purchase of ${product}`,
+//             shipping: {
+//               name: token.card.name,
+//               address: {
+//                 country: token.card.address_country,
+//               },
+//             },
+//           },
+//           { idempotencyKey }
+//         )
+//         .then((charge: Object) => {
+//           console.log({ charge });
+//         });
+//       if (!charge) {
+//         throw new Error("charge unsuccessful");
+//       }
+//     })
+//     .then((result: Object) => {
+//       // console.log({ result });
+//       res.status(200).json(result);
+//     })
+//     .catch((error: string) => {
+//       console.log({ error });
+//       res.status(500).json({
+//         error,
+//       });
+//     });
+// };
