450ffaf7f3836c6a9965c730035bf3e794b48cc9
Michal Radomski <michalra@op.pl>
2022-10-03 19:54:52 +0200

    React-Router was added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Actions from "./components/Actions";
+import List from "./components/List";
+
+const NotFound = (): JSX.Element => <h1 style={{ textAlign: "center", marginTop: "80px" }}>Page Not Found</h1>;
 
 function App(): JSX.Element {
   return (
     <React.Fragment>
-      <Actions />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Actions />} />
+          <Route path="/list" element={<List />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </Router>
     </React.Fragment>
   );
 }
---
+++ client/src/components/List.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const List = (): JSX.Element => {
+  return <React.Fragment>List</React.Fragment>;
+};
+
+export default List;
