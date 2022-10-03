088d342699028df40fe910b2794d3979ca968980
Michal Radomski <michalra@op.pl>
2022-10-03 20:43:13 +0200

    List done

--- client/src/Interfaces.d.ts
+++ client/src/Interfaces.d.ts
@@ -4,3 +4,10 @@ export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 export type Fetch = typeof store.fetch;
 export type Action = typeof store.action;
+
+export interface ListItem {
+  _id: string;
+  idempotencyKey: string;
+  amount: number | string;
+  createdAt: string | Date;
+}
--- client/src/components/List.tsx
+++ client/src/components/List.tsx
@@ -1,7 +1,50 @@
 import React from "react";
+import axios from "axios";
+import { ListItem } from "../Interfaces";
 
 const List = (): JSX.Element => {
-  return <React.Fragment>List</React.Fragment>;
+  const [list, setList] = React.useState<ListItem[]>([]);
+  // console.log({list});
+
+  const heading = ["Idempotency Key", "Amount", "Created At"];
+
+  const getList = () => {
+    axios
+      .get("/api/get-list")
+      .then((res) => setList(res.data))
+      .catch((error) => console.log({ error }));
+  };
+
+  React.useEffect(() => {
+    getList();
+  }, []);
+
+  return (
+    <div>
+      <h1>List of Payments by Card</h1>
+
+      <table style={{ width: 500 }}>
+        <thead>
+          <tr>
+            {heading.map((head) => (
+              <th key={heading.indexOf(head)}>{head}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {list.map((item) => {
+            return (
+              <tr key={item._id}>
+                <td>{item.idempotencyKey}</td>
+                <td>{item.amount}</td>
+                <td>{item.createdAt as string}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default List;
