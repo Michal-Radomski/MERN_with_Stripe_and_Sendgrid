23a3f22da530f31fb2a96fc82029c6f6add3d9f8
Michal Radomski <michalra@op.pl>
2022-10-04 20:14:30 +0200

    Header was updaed

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -6,18 +6,16 @@ const Header = (): JSX.Element => {
   let location = useLocation();
   // console.log("location.pathname:", location.pathname);
 
-  const [active] = React.useState<string>(location.pathname);
-
   return (
     <React.Fragment>
       <Nav className="justify-content-center">
         <Nav.Item>
-          <Nav.Link href="/" disabled={active === "/" ? true : false} className="navigation">
+          <Nav.Link href="/" disabled={location.pathname === "/" ? true : false} className="navigation">
             Home
           </Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link href="/list" disabled={active === "/list" ? true : false} className="navigation">
+          <Nav.Link href="/list" disabled={location.pathname === "/list" ? true : false} className="navigation">
             List
           </Nav.Link>
         </Nav.Item>
