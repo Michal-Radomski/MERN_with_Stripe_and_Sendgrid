0ee010bd79451930a4ead1619aa7daa9253c9eba
Michal Radomski <michalra@op.pl>
2022-12-17 00:31:38 +0100

    Little updates

--- client/src/i18n.ts
+++ client/src/i18n.ts
@@ -1,28 +1,28 @@
 import i18n from "i18next";
-import Backend from "i18next-http-backend";
+import HttpBackend, { HttpBackendOptions } from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 
 i18n
-  .use(Backend)
+  .use(HttpBackend)
   .use(LanguageDetector)
   .use(initReactI18next)
-  .init({
+  .init<HttpBackendOptions>({
     backend: {
       // Translation files path
       loadPath: "/i18n/{{lng}}.json",
     },
-    fallbackLng: "en",
+    lng: localStorage.getItem("i18nextLng") || "en",
     // Logs in console
     debug: false,
     // You can have multiple namespaces, in case you want to divide a huge translation into smaller pieces and load them on demand
     interpolation: {
-      espaceValue: false,
+      escapeValue: false,
       formatSeparator: ",",
     },
     react: {
       useSuspense: true,
     },
-  } as any);
+  });
 
 export default i18n;
