3fe1a6f49341ed746e40ac9557d6aa20c0d2abc8
Michal Radomski <michalra@op.pl>
2024-11-21 23:07:15 +0100

    Litte update

--- server/server.ts
+++ server/server.ts
@@ -66,7 +66,7 @@ const port = (process.env.PORT || 5000) as number;
 
 const server = http.createServer(app);
 server.listen({ port: port }, (): void => {
-  console.log(`Server is listening at http://localhost:${port}`);
+  console.log(`🚀 Server is listening at http://localhost:${port}`);
   // For testing only
   console.log("Current Time:", new Date().toLocaleTimeString());
 });
